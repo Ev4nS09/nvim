1d6153bf7464262c9fd69a1a190dda91108d73be
Ev4nS09 <afonsorio@gmail.com>
2024-09-05 22:55:32 +0100

    NeoVim Config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,40 @@
+return
+{
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd"}
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({capabilities = capabilities})
+            lspconfig.clangd.setup({capabilities = capabilities})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+            vim.fn.sign_define('DiagnosticSignError',{ text = '󰞇', texthl = 'DiagnosticSignError'})
+            vim.fn.sign_define('DiagnosticSignWarn', { text = '󱢾', texthl = 'DiagnosticSignWarn' })
+            vim.fn.sign_define('DiagnosticSignInfo', { text = '󰯂', texthl = 'DiagnosticSignInfo' })
+            vim.fn.sign_define('DiagnosticSignHint', { text = '󱘆', texthl = 'DiagnosticSignHint' })
+
+            vim.diagnostic.config({
+                    virtual_text = {
+                        prefix = '󰓥'
+                    }
+            })
+        end
+    }
+}
---
+++ lua/vim-essentials.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+
+function bt()
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
