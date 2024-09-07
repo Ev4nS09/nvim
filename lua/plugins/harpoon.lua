b97b11038d05b9de397a81ba43685c6453bdda31
Ev4nS09 <Ev4nS09>
2024-09-07 18:30:50 +0100

    Harpoon functioning

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,21 @@
 return
 {
+    'ThePrimeagen/harpoon',
+    config = function()
+        require("telescope").load_extension('harpoon')
+
+        local mark = require('harpoon.mark')
+        local ui = require('harpoon.ui')
+
+        vim.keymap.set('n', '<leader>a', mark.add_file)
+        vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+        vim.keymap.set('n', '<C-s>', function() ui.nav_file(1) end)
+        vim.keymap.set('n', '<C-k>', function() ui.nav_file(2) end)
+        vim.keymap.set('n', '<C-l>', function() ui.nav_file(3) end)
+
+        vim.keymap.set('n', '<Tab>', function() ui.nav_next() end)
+        vim.keymap.set('n', '<S-Tab>', function() ui.nav_prev() end)
+
+    end
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,6 +21,7 @@ return
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({capabilities = capabilities})
             lspconfig.clangd.setup({capabilities = capabilities})
+            --lspconfig.harper_ls.setup({capabilities = capabilities})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
