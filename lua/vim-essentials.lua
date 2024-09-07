26d1091dff43d72a558d55ee3b9d29bb6615232c
Ev4nS09 <Ev4nS09>
2024-09-07 13:59:35 +0100

    Clipboard setted to unmaedplus

--- lua/vim-essentials.lua
+++ lua/vim-essentials.lua
@@ -3,11 +3,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-
-function bt()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-end
+vim.cmd("set autoindent")
+vim.cmd("set clipboard=unnamedplus")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
