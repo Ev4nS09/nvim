0151413796d6b75d27ca2a9c855a71f21596b5ef
Ev4nS09 <Ev4nS09>
2025-02-10 11:58:17 +0000

    Java LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 return
 {
-    {
+        {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
@@ -10,9 +10,9 @@ return
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd"}
-            })
-        end
+                ensure_installed = {"lua_ls", "clangd", "jdtls"}
+        })
+    end
     },
     {
         "neovim/nvim-lspconfig",
@@ -21,6 +21,7 @@ return
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({capabilities = capabilities})
             lspconfig.clangd.setup({capabilities = capabilities})
+            lspconfig.jdtls.setup({capabilities = capabilities})
             --lspconfig.harper_ls.setup({capabilities = capabilities})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
