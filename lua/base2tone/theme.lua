d1f839be4f878bc0a1416c2bb244f49047a7b3c1
E4vnS09 <afonsorio1500@gmail.com>
2026-03-23 23:07:58 +0000

    fixing errors

--- lua/base2tone/theme.lua
+++ lua/base2tone/theme.lua
@@ -1,1 +1,1 @@
-vim.cmd("colorscheme base2tone_evening_dark")
+vim.cmd("colorscheme catppuccin-macchiato")
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return
 {
-    "nvim-treesitter/nvim-treesitter", 
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local tree_config = require("nvim-treesitter.configs") 
-            tree_config.setup({
+         require("nvim-treesitter")
+            .setup({
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
