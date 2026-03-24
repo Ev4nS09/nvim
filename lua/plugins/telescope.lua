868a434822f41f2292adf11f89832f32c04ff810
E4vnS09 <afonsorio1500@gmail.com>
2026-03-24 23:38:58 +0000

    hmm

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,13 @@ return
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+    },
+    config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+        end
 }
 
 
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -3,6 +3,7 @@ return
     'xiyaowong/transparent.nvim',
     config = function()
         require('transparent').clear_prefix('NeoTree')
+        require('transparent').clear_prefix('telescope')
         --require('transparent').clear_prefix('lualine')
     end
 }
